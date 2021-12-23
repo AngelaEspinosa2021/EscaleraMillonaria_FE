b1c530e46b7e2a50e152ae44cdfec8db4d10e15b
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-23 15:52:58 -0500

    creacion del Interface question.model.ts dentro del modulo Shared

---
+++ EscaleraMillonariaFrontend/src/app/shared/component/models/question.model.ts
@@ -0,0 +1,9 @@
+export interface Question {
+    idQuestion: number,
+    QuestionStatement: string,
+    OptionOne: string,
+    OptionTwo: string,
+    OptionThree: string,
+    OptionFour: string,
+    IdCategory: number 
+}
