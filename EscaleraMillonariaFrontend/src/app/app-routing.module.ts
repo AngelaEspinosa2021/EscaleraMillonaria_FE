a10ee5dab18607f873894ea75581116ba771690c
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-23 15:08:42 -0500

    importando QuestionsComponent dentro del app-routing.module

--- EscaleraMillonariaFrontend/src/app/app-routing.module.ts
+++ EscaleraMillonariaFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'questions',
+    component: QuestionsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
