84e5a430b6164ebecd1089f70a97ad4dcf54ebcd
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-23 15:40:28 -0500

    llamada del EndPoint dentro de QuestionsServices

--- EscaleraMillonariaFrontend/src/app/core/services/questions/questions.service.ts
+++ EscaleraMillonariaFrontend/src/app/core/services/questions/questions.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionsService {
 
+  baserUrl: string = 'https://localhost:44373/api/Questions';
+
   constructor(private http: HttpClient) { }
 
-  getQuestionsByCategory(){
-
+  getQuestionsByCategory(idCategory: number){
+    return this.http.get<Question[]>(`${environment.url_api}/questions/${idCategory}`);
   }
 }
