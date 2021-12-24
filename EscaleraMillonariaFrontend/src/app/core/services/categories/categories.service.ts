89fa31e67c586e925887f30077c53b634bf8100a
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-12-24 17:30:40 -0500

    importacion de CategoriesServices dentro de Question.Component.ts - creacion del metodo que recibe la lista de categorias para mostrarla en nuestro html. - se hace prueba y la data se recibe correctamente mostrandose  en nuestro html

--- EscaleraMillonariaFrontend/src/app/core/services/categories/categories.service.ts
+++ EscaleraMillonariaFrontend/src/app/core/services/categories/categories.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 
-import { Category } from 'src/app/shared/models/category.model';
-
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +11,6 @@ export class CategoriesService {
   constructor(private http: HttpClient) { }
 
   getAllCategories(){
-    return this.http.get<Category[]>(`${environment.url_api}/Categories`);
+    return this.http.get<any>(`${environment.url_api}/Categories`);
   }
 }
--- EscaleraMillonariaFrontend/src/app/questions/questions.component.ts
+++ EscaleraMillonariaFrontend/src/app/questions/questions.component.ts
@@ -3,8 +3,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Params } from '@angular/router';
 import { QuestionsService } from '../core/services/questions/questions.service';
+import { CategoriesService } from '../core/services/categories/categories.service';
 
-
+import { Category } from '../shared/models/category.model';
 import { Question } from './../shared/models/question.model';
 
 @Component({
@@ -14,24 +15,29 @@ import { Question } from './../shared/models/question.model';
 })
 export class QuestionsComponent implements OnInit {
 
+  categories: Category[] = [];
+  currentCategory?: Category = undefined;
+  indexCategory = 0;
   questions: Question[] = [];
   currentQuestion?: Question = undefined;
-  index = 0;
+  indexQuestion = 0;
     
   constructor(
     private questionsService: QuestionsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private categoriesService: CategoriesService
     ) { }
 
   ngOnInit(): void {
-    this.getQuestionsByCategory(1);   
+    this.getQuestionsByCategory(1);
+    this.getAllCategories(); 
   }
 
   getQuestionsByCategory(idCategory: number) {
     this.questionsService.getQuestionsByCategory(idCategory).subscribe(questions => {
       {
         this.questions = questions.result;
-        this.currentQuestion = this.questions[this.index];
+        this.currentQuestion = this.questions[this.indexQuestion];
         console.log(questions); 
       }
     });     
@@ -42,10 +48,18 @@ export class QuestionsComponent implements OnInit {
     
     if(num == answer)
     {
-      this.index++;
-      this.currentQuestion = this.questions[this.index];
+      this.indexQuestion++;
+      this.currentQuestion = this.questions[this.indexQuestion];
     }
     console.log(false);
+  }
+
+  getAllCategories(){
+    this.categoriesService.getAllCategories().subscribe(categories => {
+      this.categories = categories.result;
+      this.currentCategory = this.categories[this.indexCategory];      
+      console.log(categories);
+    })
   }
 
 }
